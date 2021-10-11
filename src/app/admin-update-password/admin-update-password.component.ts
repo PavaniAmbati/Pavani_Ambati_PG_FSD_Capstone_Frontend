28121557fc499798567ff256903ede97bbbbaaeb
Pavani <pavanisk@gmail.com>
2021-10-11 19:20:06 -0400

    added update password functionality. User can update password if password entered matches with repeat password. User is taken back to login screen to login with updated password.

--- src/app/admin-update-password/admin-update-password.component.ts
+++ src/app/admin-update-password/admin-update-password.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AdminuserService } from '../adminuser.service';
 
 @Component({
   selector: 'app-admin-update-password',
   templateUrl: './admin-update-password.component.html',
-  styleUrls: ['./admin-update-password.component.css']
+  styleUrls: ['./admin-update-password.component.css'],
+  providers: [AdminuserService]
 })
 export class AdminUpdatePasswordComponent implements OnInit {
 
-  constructor() { }
+  public adminuser: any = [];
+  public adminusers: any = [];
+  public id!: string;
+  @Input() users = { Newpassword: '', Repeatpassword: '' }
+  errorMessage = 'Passwords does not match. Try again';
+  Passwordnotmatch = false;
+  username = this.actRoute.snapshot.params['Username'];
+
+  constructor(
+    public restApi: AdminuserService,
+    public router: Router,
+    public actRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.loadadminusers();
+  }
+
+  loadadminusers() {
+    return this.restApi.getUsers().subscribe((data: {}) => {
+      this.adminusers = data;
+      console.log(this.adminusers);
+    })
+
+  }
+
+  updatePassword(users: any) {
+
+    if (users.Newpassword === users.Repeatpassword) {
+      this.Passwordnotmatch = false;
+
+      console.log(this.username)
+      console.log(this.users.Repeatpassword)
+
+      this.adminuser = { 
+        "username": this.username, 
+        "password": this.users.Repeatpassword 
+      }
+      this.restApi.updateUser(this.username, this.adminuser).subscribe(data => {
+            this.router.navigate(['/adminlogin'])
+          }) 
+
+        }else {
+      this.Passwordnotmatch = true;
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('localUser');
+    this.router.navigate(['/adminlogin']);
+
   }
 
 }
